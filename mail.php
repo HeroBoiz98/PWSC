879de28b7a03d7dbe58a69b090a63d4612ba021a
HeroBoiz98 <105982952+HeroBoiz98@users.noreply.github.com>
2022-06-01 11:39:50 +0530

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,15 @@
+<?php
+//get data from form  
+$name = $_POST['name'];
+$email= $_POST['email'];
+$message= $_POST['message'];
+$to = "adityashukla832@gmail.com";
+$subject = "Mail From user";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+$headers = "From: noreply@aditya.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("index.html");
+?>
